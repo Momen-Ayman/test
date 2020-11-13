17c5c6a8a8cd5deea5ea57b1b6f87b2ad65c2a0c
Momen-Ayman <57829667+Momen-Ayman@users.noreply.github.com>
2020-11-13 13:28:00 -0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -4,24 +4,21 @@ $(function () {
   });
 }); //]]>
 
-window.onload = function(){
-    $('#title').html(
-        $('.btn1').text()
-    )
-}
-    
-$('button').click((e)=>{
-    $('#title').html(e.target.innerHTML);
-    if(e.target.classList.value == "btn1"){
-        $('video').attr('src' , '../Videos/1⃣4⃣.mp4');
-    }
-    else if(e.target.classList.value == "btn2"){
-        $('video').attr('src' , '../Videos/1⃣4⃣.mp4');
-    }
-    else if(e.target.classList.value == "btn3"){
-        $('video').attr('src' , '');
-    }
-    else if(e.target.classList.value == "btn4"){
-        $('video').attr('src' , '');
-    }
-})
+window.onload = function () {
+  $("#title").html($(".btn1").text());
+};
+
+$("button").click((e) => {
+  $("#title").html(e.target.innerHTML);
+  if (e.target.classList.value == "btn1") {
+    $("video").attr("src", "../Videos/القراءه 5 Ss-1.mp4");
+  } else if (e.target.classList.value == "btn2") {
+    $("video").attr("src", "../Videos/4 ألواجب Ss-1.mp4");
+  } else if (e.target.classList.value == "btn3") {
+    $("video").attr("src", "../Videos/3 بلاغة الجزء الثاني Ss-1.mp4");
+  } else if (e.target.classList.value == "btn4") {
+    $("video").attr("src", "../Videos/2 بلاغة الجزء الثاني Ss-1.mp4");
+  } else if (e.target.classList.value == "btn5") {
+    $("video").attr("src", "../Videos/1 نحو Ss-1.mp4");
+  }
+});
