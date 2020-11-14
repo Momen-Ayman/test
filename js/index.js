d2691ce02a39c517c248e84c2249626106b321f3
Momen-Ayman <57829667+Momen-Ayman@users.noreply.github.com>
2020-11-13 16:51:32 -0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -11,14 +11,14 @@ window.onload = function () {
 $("button").click((e) => {
   $("#title").html(e.target.innerHTML);
   if (e.target.classList.value == "btn1") {
-    $("video").attr("src", "https://media.githubusercontent.com/media/Momen-Ayman/test/main/../Videos/القراءه 5 Ss-1.mp4");
+    $("video").attr("src", "https://media.githubusercontent.com/media/Momen-Ayman/test/main/./Videos/القراءه 5 Ss-1.mp4");
   } else if (e.target.classList.value == "btn2") {
-    $("video").attr("src", "https://media.githubusercontent.com/media/Momen-Ayman/test/main/../Videos/4 ألواجب Ss-1.mp4");
+    $("video").attr("src", "https://media.githubusercontent.com/media/Momen-Ayman/test/main/./Videos/4 ألواجب Ss-1.mp4");
   } else if (e.target.classList.value == "btn3") {
-    $("video").attr("src", "https://media.githubusercontent.com/media/Momen-Ayman/test/main/../Videos/3 بلاغة الجزء الثاني Ss-1.mp4");
+    $("video").attr("src", "https://media.githubusercontent.com/media/Momen-Ayman/test/main/./Videos/3 بلاغة الجزء الثاني Ss-1.mp4");
   } else if (e.target.classList.value == "btn4") {
-    $("video").attr("src", "https://media.githubusercontent.com/media/Momen-Ayman/test/main/../Videos/2 بلاغة الجزء الثاني Ss-1.mp4");
+    $("video").attr("src", "https://media.githubusercontent.com/media/Momen-Ayman/test/main/./Videos/2 بلاغة الجزء الثاني Ss-1.mp4");
   } else if (e.target.classList.value == "btn5") {
-    $("video").attr("src", "https://media.githubusercontent.com/media/Momen-Ayman/test/main/../Videos/1 نحو Ss-1.mp4");
+    $("video").attr("src", "https://media.githubusercontent.com/media/Momen-Ayman/test/main/./Videos/1 نحو Ss-1.mp4");
   }
 });
